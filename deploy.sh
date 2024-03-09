5fe8ce61f5472dc412de446cab254bda288aa402
nhnghia272 <nhnghia272@gmail.com>
2024-03-09 23:45:56 +0700

    init frontend

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,16 @@
 docker-compose down
+docker rmi -f viact_backend
+docker rmi -f viact_frontend
 
 cd backend
 npm install
 rm -rf dist
 npm run build
 
+cd ../frontend
+npm install
+rm -rf dist
+npm run build
+
 cd ..
-docker rmi -f viact_frontend
-docker rmi -f viact_backend
 docker-compose up -d
